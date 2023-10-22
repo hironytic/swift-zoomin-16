76e6ae5bf9ccee97f36b76e00115a3223f18caf2
Hironori Ichimiya <hiron@hironytic.com>
2023-10-22 19:12:10 +0900

    ロードの度に名前をランダムに変更

--- Zoomin/UserRepository.swift
+++ Zoomin/UserRepository.swift
@@ -17,11 +17,30 @@ struct UserRepository: UserRepositoryProtocol {
     
     func fetchValue(for id: User.ID) async throws -> User? {
         try await Task.sleep(nanoseconds: 2_000_000_000)
-        return values[id]
+        guard var user = values[id] else { return nil }
+        user.name = generateRandomUserName()
+        return user
     }
     
     func fetchAllValues() async throws -> [User] {
         try await Task.sleep(nanoseconds: 2_000_000_000)
-        return values.values.sorted(by: { $0.id.rawValue < $1.id.rawValue })
+        return values
+            .values
+            .sorted(by: { $0.id.rawValue < $1.id.rawValue })
+            .map {
+                var user = $0
+                user.name = generateRandomUserName()
+                return user
+            }
+    }
+    
+    private func generateRandomUserName() -> String {
+        let adjectives = ["Happy", "Silly", "Clever", "Cool", "Funky", "Lucky", "Gentle", "Brave", "Wild", "Dizzy"]
+        let nouns = ["Cat", "Dog", "Unicorn", "Penguin", "Ninja", "Pirate", "Wizard", "Dragon", "Alien", "Robot"]
+        
+        let randomAdjective = adjectives.randomElement() ?? "Random"
+        let randomNoun = nouns.randomElement() ?? "User"
+        
+        return randomAdjective + randomNoun
     }
 }
